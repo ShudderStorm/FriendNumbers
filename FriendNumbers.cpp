d506e4a005febc81efc67a0517bafdce9d4a1843
ShudderStorm <115107620+ShudderStorm@users.noreply.github.com>
2022-10-09 19:05:19 +0500

    Add files via upload

---
+++ FriendNumbers.cpp
@@ -0,0 +1,33 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+	int a; cin >> a;
+	int b; cin >> b;
+
+	if (a <= 0 || b <= 0) {
+		cout << "Error: number(s) is(are) not natural";
+	}
+	int k = 0;
+	int l = 0;
+
+	for (int i = 1; i < a; i++) {
+		if (a % i == 0) {
+			k += i;
+		}
+	}
+	for (int i = 1; i < b; i++) {
+		if (b % i == 0) {
+			l += i;
+		}
+	}
+	if (k == b && l == a) {
+		cout << "Numbers are friends";
+	}
+	else {
+		cout << "Numbers are not friends";
+	}
+	return 0;
+}
+
